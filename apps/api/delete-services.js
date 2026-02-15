ab430a9819f57526e976555345d5a07ab4ef7bf6
hhaai <nguyenhoanghai861@gmail.com>
2026-02-15 20:26:37 +0700

    (credentials) Fix MongoDB Atlas Database URI with credentials

--- apps/api/delete-services.js
+++ apps/api/delete-services.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb+srv://nail_salon_api_user:8RXKNkNpn8BLmhc5@cluster0.m6ia2tj.mongodb.net/nail-salon-dev?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI;
 
 async function deleteServices() {
   const client = new MongoClient(uri);
